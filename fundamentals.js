87eb97cb36edf49a3faed9eabfbd1dd300d38342
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-09-18 21:46:37 +0100

    Nested objects

--- fundamentals.js
+++ fundamentals.js
@@ -1,2 +1,11 @@
- const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
+ const aCar = {
+        owner : "Joe Bloggs",
+        type :{
+            make:'Toyota',
+            model: 'Corolla',
+            CC: ' 1.8' 
+        },
+        registration : '201WD1058' };
+
 console.log(aCar.owner);
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
