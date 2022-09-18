1a81ea3309dc101f60f48400fdff23fd1e6bd822
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-09-18 22:09:58 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -7,5 +7,17 @@
         },
         registration : '201WD1058' };
 
+        
+//Add new property
+aCar.mileage = '1000';
+aCar.colour = {
+    exterior:'red',
+    interior:{
+        texture: 'leather',
+        shade: 'cream' 
+    }
+}
+
+console.log('it is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior')
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
